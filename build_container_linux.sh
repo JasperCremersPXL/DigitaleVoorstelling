bcfae46c8011e7cabb2626a653881d1f4c7dd082
jaspe <jasper.cremers@brainjar.ai>
2022-04-30 16:03:00 +0200

    final push

--- build_container_linux.sh
+++ build_container_linux.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cd FixUnityProject/ &&
 git submodule update --init --recursive &&
 docker build -t unity-robotics:pick-and-place -f docker/Dockerfile .
--- start_container_linux.sh
+++ start_container_linux.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 docker run -it --rm -p 10000:10000 unity-robotics:pick-and-place /bin/bash
